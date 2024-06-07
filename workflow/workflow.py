71c5044722dcd4421f7c29cef2ec30f21f86a379
xsqian <xsqian@gmail.com>
2024-06-07 22:05:37 +0000

    get_function and then run the function

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -8,12 +8,14 @@ in an orchestrated manner
 from kfp import dsl
 import mlrun
 
-
 # Create a Kubeflow Pipelines pipeline
 @dsl.pipeline(name="testing-resource")
 def test():
+    project = mlrun.get_current_project()
+    func = project.get_function("nodegroup-test")
     run2 = mlrun.run_function(
-        name="nodegroup-test",
-        function="nodegroup-test",
+        name="nodegroup-test-run-from-workflow",
+        function=func,
+        auto_build=False,
         local=False,
     )
